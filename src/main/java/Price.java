c3c9593e39b53244e7cad1332acd9ceb1231c950
maTh51 <matheustigon@gmail.com>
2020-10-19 22:09:44 -0300

    Commit 14

--- src/main/java/Price.java
+++ src/main/java/Price.java
@@ -13,5 +13,14 @@ public abstract class Price {
     public abstract int getPriceCode();
 
     public abstract double getCharge(int daysRented);
+    
+    public int getFrequentRenterPoints(int daysRented) {
+        int frequentRenterPoints = 1;
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == Movie.NEW_RELEASE)   &&
+            daysRented > 1) frequentRenterPoints ++;
+     
+        return frequentRenterPoints;
+    }
 
 }
