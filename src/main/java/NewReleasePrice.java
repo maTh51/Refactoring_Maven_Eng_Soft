092963d877811103a6685377ee12ba21ccc6c760
maTh51 <matheustigon@gmail.com>
2020-10-19 22:07:24 -0300

    Commit 13

--- src/main/java/NewReleasePrice.java
+++ src/main/java/NewReleasePrice.java
@@ -9,7 +9,12 @@
  * @author matheus
  */
 public class NewReleasePrice extends Price {
+    
    public int getPriceCode() {
        return Movie.NEW_RELEASE;
    }
+   
+   public double getCharge(int daysRented){
+      return daysRented * 3;
+   } 
 }
