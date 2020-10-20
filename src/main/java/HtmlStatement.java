6e9d9ce0300d17102539964e57f3388ca71a4323
maTh51 <matheustigon@gmail.com>
2020-10-19 22:40:43 -0300

    Commit 16

---
+++ src/main/java/HtmlStatement.java
@@ -0,0 +1,33 @@
+
+import java.util.Enumeration;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author matheus
+ */
+public class HtmlStatement extends Statement {
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
+        "</EM></H1><P>\n";
+        while (rentals.hasMoreElements()) {
+           Rental each = (Rental) rentals.nextElement();
+           //show figures for each rental
+           result += each.getMovie().getTitle()+ ": " +
+           String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+        //add footer lines
+        result += "<P>You owe <EM>" +
+        String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" + 
+        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+        "</EM> frequent renter points<P>";
+        return result;
+    }
+}
---
+++ src/main/java/Statement.java
@@ -0,0 +1,18 @@
+
+import java.util.Enumeration;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author matheus
+ */
+import java.util.Enumeration;
+
+public class Statement {
+
+}
